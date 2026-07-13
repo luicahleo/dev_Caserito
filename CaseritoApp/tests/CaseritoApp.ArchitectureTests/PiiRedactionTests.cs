e29e3b355fcfa664477ce8c463b03e09216b14c1
Luis Rolando Cahuana León <luisrolando.cahuana@madic.com>
2026-07-13 19:23:58 +0200

    test: ampliar cobertura de PiiRedaction (campos faltantes + case-insensitivity)

--- CaseritoApp/tests/CaseritoApp.ArchitectureTests/PiiRedactionTests.cs
+++ CaseritoApp/tests/CaseritoApp.ArchitectureTests/PiiRedactionTests.cs
@@ -7,10 +7,23 @@ public sealed class PiiRedactionTests
 {
     [Theory]
     [InlineData("ci")]
+    [InlineData("documentoNumero")]
+    [InlineData("documentoImagen")]
     [InlineData("selfie")]
     [InlineData("token")]
     [InlineData("qr")]
+    [InlineData("pagoReferencia")]
     public void Redactar_enmascara_campos_prohibidos(string campo)
+    {
+        Assert.Equal("***", PiiRedaction.Redactar(campo, "valor-sensible"));
+    }
+
+    [Theory]
+    [InlineData("CI")]
+    [InlineData("Token")]
+    [InlineData("QR")]
+    [InlineData("SelFie")]
+    public void Redactar_enmascara_campos_prohibidos_sin_distinguir_mayusculas(string campo)
     {
         Assert.Equal("***", PiiRedaction.Redactar(campo, "valor-sensible"));
     }
